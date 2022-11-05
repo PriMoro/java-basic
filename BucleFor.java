943cb73842dd817145e29aa6b0a7cfb748372714
PriMoro <86128822+PriMoro@users.noreply.github.com>
2022-11-05 18:09:17 -0300

    loops examples

---
+++ BucleFor.java
@@ -0,0 +1,52 @@
+public class BucleFor {
+    public static void main(String args[]) {
+        for (int x = 0; x < 10; x++) {
+            System.out.println("valor de x: " + (x));
+        }
+
+        System.out.print("\n");
+        System.out.print("-----");
+        System.out.print("\n");
+
+        int[] numbers = { 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+
+        for (int number : numbers) {
+            System.out.print(number);
+            System.out.print("\n");
+
+        }
+        System.out.print("\n");
+        System.out.print("-----");
+        System.out.print("\n");
+        String[] names = { "clara", "juan", "paco", "pedro", "marta" };
+
+        for (String name : names) {
+            System.out.print(name);
+            System.out.print("\n");
+        }
+
+        System.out.print("\n");
+        System.out.print("-----");
+        System.out.print("\n");
+
+        for (int number : numbers) {
+            if (number == 50) {
+                continue;
+            }
+            System.out.print(number);
+            System.out.println("\n");
+        }
+
+        System.out.print("\n");
+        System.out.print("-----");
+        System.out.print("\n");
+
+        for (int number : numbers) {
+            if (number == 50) {
+                break;
+            }
+            System.out.print(number);
+            System.out.println("\n");
+        }
+    }
+}
--- Test.java
+++ Test.java
@@ -1,3 +1,27 @@
 public class Test {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 15;
+        int c = 20;
+        int d = 25;
 
+        int x = 20;
+
+        System.out.println("a + (b * c) - d = " + (a + b * c - d));
+        System.out.println("a == b : " + (a == b));
+        System.out.println("c < d : " + (c < d));
+
+        while (x < 30) {
+            System.out.println("valor de x: " + (x));
+            x++;
+        }
+
+        do {
+            System.out.print(x);
+            x++;
+            System.out.print("\n");
+            System.out.print("\n");
+        } while (x < 20);
+
+    }
 }
