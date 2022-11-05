7ee01a64b29481db97536fe26bcb11fe0dff7722
PriMoro <86128822+PriMoro@users.noreply.github.com>
2022-11-05 17:16:10 -0300

    example 2 - java

---
+++ Inquilino.java
@@ -0,0 +1,10 @@
+public class Inquilino {
+    private static String nombre_inquilino;
+
+    public static void main(String[] args) {
+
+        Inquilino.nombre_inquilino = "Pri";
+
+        System.out.println("hola " + Inquilino.nombre_inquilino);
+    }
+}
